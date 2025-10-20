0bfe5b1db54d96822ea3260222a815777fce23ea
Walmir Alves <walmirjr915@gmail.com>
2025-10-20 15:37:15 -0300

    Project created

---
+++ src/main/java/com/walmir/gestorfinanceiro/GestorfinanceiroApplication.java
@@ -0,0 +1,13 @@
+package com.walmir.gestorfinanceiro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestorfinanceiroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestorfinanceiroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/walmir/gestorfinanceiro/GestorfinanceiroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.walmir.gestorfinanceiro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestorfinanceiroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
